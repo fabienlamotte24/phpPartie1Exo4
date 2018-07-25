28fd5081cca968e0a96acf44d5d85d393944559a
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 14:21:15 +0200

    php partie 1 exo 4

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 4</title>
+  </head>
+  <body>
+    <?php
+      $string = 'Ceci est une chaîne de caractère';
+      $int = 5;
+      $float = 5.3;
+      $boolean = true;
+      echo $string . ' !<br />' . $int . ' !<br />' . $float . ' !<br />' . $boolean . ' !';
+    ?>
