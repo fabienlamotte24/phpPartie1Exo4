5bdabc398957b5648f4ee8c3c991280c773f90c3
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 15:27:10 +0200

    mise en commentaire de l'exercice

--- index.php
+++ index.php
@@ -7,10 +7,15 @@
   <body>
     <p>
       <?php
+      /*Déclaration d'une variable en chaîne de caractère*/
       $string = 'Ceci est une chaîne de caractère';
+      /*Déclaration d'une variable en int(nombre entier)*/
       $int = 5;
+      /*Déclaration d'une variable en nombre à virgule(float)*/
       $float = 5.3;
+      /*Déclaration d'une variable en boolean(true or false)*/
       $boolean = true;
+      /*Affichage des variables*/
       echo $string . ' , ' . var_dump($int) .  $float . ' , ' . $boolean;
       ?>
     </p>
